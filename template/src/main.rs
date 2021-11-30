057ee624559143228f8442405eca735d33da4c77
redwing <redwing_bb@protonmail.com>
2021-11-30 10:18:21 -0500

    template is ready, added gitignore file

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
+// https://adventofcode.com/2021/day/1
+
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+fn main() -> io::Result<()> {
+
+    let file = File::open("input.txt")?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+    }
+
     println!("Hello, world!");
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests { 
+    use super::*;
+
+    #[test]
+    fn test_1() { assert_eq!(4 + 4, 8); }
 }
