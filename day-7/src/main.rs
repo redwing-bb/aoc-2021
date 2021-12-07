e1da3fb4d8f18008796433d62d59b640eabc2d3c
redwing <redwing_bb@protonmail.com>
2021-12-07 12:56:28 -0500

    p2, bruteforce but it gets there

--- day-7/src/main.rs
+++ day-7/src/main.rs
@@ -9,27 +9,39 @@ fn main() {
 
     // read input
     let input = fs::read_to_string(FILE).expect("no u");
-    let crabs: Vec<u32> = input.trim().split(",")
+    let crabs: Vec<u64> = input.trim().split(",")
         .map(|f| f.parse().unwrap())
         .collect();
 
     let max = crabs.iter().max().unwrap();
-    let min = crabs.iter().min().unwrap();
-    let mean:u32 = crabs.iter().sum::<u32>() / crabs.len() as u32;
+    let mean:u64 = crabs.iter().sum::<u64>() / crabs.len() as u64;
 
-    let mut best: u32 = *max * crabs.len() as u32;
+    let mut best: u64 = *max * crabs.len() as u64;
     for p in 0..=mean {
-        let f = fuel_to_reach(p as u32, &crabs);
+        let f = fuel_to_reach(p as u64, &crabs);
         if f < best {
             best = f;
         }
     }
 
-    println!("{}", best);
+    println!("{}", best); // part 1
+
+
+    let half = (*max / 2) + 1;
+    best = (half * (half+1))/2 * crabs.len() as u64;
+    for p in 0..=best {
+        let f = fuel_to_reach_p2(p as u64, &crabs);
+        if f < best {
+            best = f;
+        }
+        println!("{}, {}", best, f);
+    }
+
+    println!("{}", best); // part 2
 }
 
-fn fuel_to_reach(position: u32, crabs: &Vec<u32>) -> u32 {
-    let mut sum: u32 = 0;
+fn fuel_to_reach(position: u64, crabs: &Vec<u64>) -> u64 {
+    let mut sum: u64 = 0;
     for crab in crabs.iter() {
         if crab > &position {
             sum += crab-position;
@@ -40,6 +52,21 @@ fn fuel_to_reach(position: u32, crabs: &Vec<u32>) -> u32 {
     sum
 }
 
+
+fn fuel_to_reach_p2(position: u64, crabs: &Vec<u64>) -> u64 {
+    let mut sum: u64 = 0; 
+    for crab in crabs.iter() {
+        let mut d = 0;
+        if crab > &position {
+            d = crab-position;
+        } else if crab < &position {
+            d = position-crab;
+        } 
+
+        if d > 0 { sum += (d * (d+1))/2 }
+    }
+    sum
+}
 
 #[cfg(test)]
 mod tests { 
