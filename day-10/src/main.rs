5d70e99ff9e240b5afe8170c143a3d682f10519b
redwing <redwing_bb@protonmail.com>
2021-12-10 20:51:39 -0500

    d10p2 done

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -8,18 +8,20 @@ const FILE: &str = "input.txt";
 
 fn main() {
 
-    /*
-): 3 points.
-]: 57 points.
-}: 1197 points.
->: 25137 points.
-    */
-
     let mut scores: HashMap<char, u32> = HashMap::new();
     scores.insert(')', 3);
     scores.insert(']', 57);
     scores.insert('}', 1197);
     scores.insert('>', 25137);
+
+    let mut scores2: HashMap<char, u64> = HashMap::new();
+    scores2.insert(')', 1);
+    scores2.insert(']', 2);
+    scores2.insert('}', 3);
+    scores2.insert('>', 4);
+
+    let mut score2_list: Vec<u64> = Vec::new();
+
     // read input
     let input = fs::read_to_string(FILE).expect("no u");
 
@@ -31,17 +33,35 @@ fn main() {
             if c == '(' { ends.push(')'); } else
             if c == '[' { ends.push(']'); } else
             if c == '{' { ends.push('}'); } else
-            if c == '<' { ends.push('>'); } else {
+            if c == '<' { ends.push('>');
+            } else {
                 let e = ends.pop().unwrap();
                 if e != c {
-                    println!("unmatched, expected {}, found {}", e, c);
-                    score += scores.get(&c).clone().unwrap();
+
+                    //println!("unmatched, expected {}, found {}", e, c);
+                    score += scores.get(&c).unwrap();
+                    ends.clear();
                     break
                 }
             }
         }
+        // ======= part 2 =======
+        let mut score2: u64 = 0;
+        ends.reverse();
+        for e in ends {
+            score2 *= 5;
+            score2 += scores2.get(&e).unwrap();
+        }
+        if score2 > 0 {
+            score2_list.push(score2);
+        }
+
+ 
     }
-    println!("{}", score);
+    println!("{}", score); // part 1
+
+    score2_list.sort();
+    println!("{}", score2_list[score2_list.len() / 2]); // part 2
 }
 
 
